b7bb0c1ceed6389f76d68c3dea1a27f0e8c76bc5
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-03-01 23:44:26 +0300

    refactor: move blog metadata to the gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Personal blog by Alex Yackovlev',
+    description: 'Thoughts on code',
+    author: `Alex Yackovlev`,
     social: [
       {
         icon: 'github',
